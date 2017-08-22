14286aa28c248458b6ed85db77068ea5c2ecabcf
Aaron Arredondo Sanchez <binauralvoice@gmail.com>
2017-08-22 15:22:43 -0500

    new ip

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ bot.hear([/(hola|holi|hello|hi)( bot| chatbot| amigo| papu)?/i], (payload, chat)
 bot.hear([/(quiero|prepara(me)?|make( me)?)?( un| a)?(caf[ée]|coffe)( papu| amigo)?/i], (payload, chat) => {
 	// Send a text message followed by another text message that contains a typing indicator
 	chat.say('Preparando...').then(() => {
-		request.post('http://192.168.0.147/makeCoffe');
+		request.post('http://172.20.10.2/makeCoffe');
 		chat.say('Que tal? :D', { typing: true });
 		console.log("Preparando tu cafe");
 	});
@@ -27,7 +27,7 @@ bot.hear([/(quiero|prepara(me)?|make( me)?)?( un| a)?(caf[ée]|coffe)( papu| ami
 bot.hear([/(turn )?((encender|enciende)|on)( the| el)?( led)?/i], (payload, chat) => {
 	// Send a text message followed by another text message that contains a typing indicator
 	chat.say('Encendiendo LED').then(() => {
-		request.post('http://192.168.0.147/ledON');
+		request.post('http://172.20.10.2/ledON');
 		chat.say('lol', { typing: true });
 	});
 });
@@ -35,7 +35,7 @@ bot.hear([/(turn )?((encender|enciende)|on)( the| el)?( led)?/i], (payload, chat
 bot.hear([/(turn )?(apaga(r)?|off)( the| el)?( led)?/i], (payload, chat) => {
 	// Send a text message followed by another text message that contains a typing indicator
 	chat.say('Apagando LED').then(() => {
-		request.post('http://192.168.0.147/ledOFF');
+		request.post('http://172.20.10.2/ledOFF');
 		chat.say(':(', { typing: true });
 	});
 });
@@ -43,7 +43,7 @@ bot.hear([/(turn )?(apaga(r)?|off)( the| el)?( led)?/i], (payload, chat) => {
 bot.hear(['blink', 'parpadear', /blink( led)?/i], (payload, chat) => {
 	// Send a text message followed by another text message that contains a typing indicator
 	chat.say('Blinking LED').then(() => {
-		request.post('http://192.168.0.147/blink');
+		request.post('http://172.20.10.2/blink');
 		chat.say(':O', { typing: true });
 	});
 });
