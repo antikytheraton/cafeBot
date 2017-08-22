d557ec45b4b98e6ad409268ffab552be2a0af8e0
Aaron Arredondo Sanchez <binauralvoice@gmail.com>
2017-08-22 16:57:40 -0500

    la prueba de la muerte

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const BootBot = require('bootbot');
 const request = require('request');
 
 const bot = new BootBot({
-  accessToken: 'ACCESS_TOKEN',
-  verifyToken: 'VERIFY_TOKEN',
-  appSecret: 'APP_SECRET'
+  accessToken: 'EAAEiGPuQ5ywBAOB1ym3kCkvCgbT7B5W9UqBaHLl3iRkw7RkfVgZAIKSLXAvfSZCV2WLYxdRhvGt3ZAcgncDfT8jrtSc6kNL3TZCUR3gOal7T0E79VWtEd68S0fl1FzMfPlPzlL5SeBcm55VECEhwFCXqNeK3doMvNH6fzlTRJXbWlNbUaOZAX',
+  verifyToken: 'un cafe papu',
+  appSecret: '14080078491f6e5e5feb0429ced912e8'
 });
 
 bot.hear([/(hola|holi|hello|hi)( bot| chatbot| amigo| papu)?/i], (payload, chat) => {
@@ -18,7 +18,7 @@ bot.hear([/(hola|holi|hello|hi)( bot| chatbot| amigo| papu)?/i], (payload, chat)
 bot.hear([/(quiero|prepara(me)?|make( me)?)?( un| a)?(caf[ée]|coffe)( papu| amigo)?/i], (payload, chat) => {
 	// Send a text message followed by another text message that contains a typing indicator
 	chat.say('Preparando...').then(() => {
-		request.post('http://172.20.10.2/makeCoffe');
+		request.post('http://192.168.43.203/makeCoffe');
 		chat.say('Que tal? :D', { typing: true });
 		console.log("Preparando tu cafe");
 	});
@@ -27,7 +27,7 @@ bot.hear([/(quiero|prepara(me)?|make( me)?)?( un| a)?(caf[ée]|coffe)( papu| ami
 bot.hear([/(turn )?((encender|enciende)|on)( the| el)?( led)?/i], (payload, chat) => {
 	// Send a text message followed by another text message that contains a typing indicator
 	chat.say('Encendiendo LED').then(() => {
-		request.post('http://172.20.10.2/ledON');
+		request.post('http://192.168.43.203/ledON');
 		chat.say('lol', { typing: true });
 	});
 });
@@ -35,7 +35,7 @@ bot.hear([/(turn )?((encender|enciende)|on)( the| el)?( led)?/i], (payload, chat
 bot.hear([/(turn )?(apaga(r)?|off)( the| el)?( led)?/i], (payload, chat) => {
 	// Send a text message followed by another text message that contains a typing indicator
 	chat.say('Apagando LED').then(() => {
-		request.post('http://172.20.10.2/ledOFF');
+		request.post('http://192.168.43.203/ledOFF');
 		chat.say(':(', { typing: true });
 	});
 });
@@ -43,7 +43,7 @@ bot.hear([/(turn )?(apaga(r)?|off)( the| el)?( led)?/i], (payload, chat) => {
 bot.hear(['blink', 'parpadear', /blink( led)?/i], (payload, chat) => {
 	// Send a text message followed by another text message that contains a typing indicator
 	chat.say('Blinking LED').then(() => {
-		request.post('http://172.20.10.2/blink');
+		request.post('http://192.168.43.203/blink');
 		chat.say(':O', { typing: true });
 	});
 });
